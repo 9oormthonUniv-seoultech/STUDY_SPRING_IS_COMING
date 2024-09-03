7d940c88874251948d19831d22b65dfc5f0cb918
hawardShin <joushin@student.42seoul.kr>
2024-09-03 18:41:36 +0900

    Init: 중은 프로젝트 세팅

---
+++ Practice/신중은/spring-with-aws/src/main/java/com/jojoidu/book/springwithaws/SpringWithAwsApplication.java
@@ -0,0 +1,13 @@
+package com.jojoidu.book.springwithaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithAwsApplication.class, args);
+	}
+
+}
---
+++ Practice/신중은/spring-with-aws/src/test/java/com/jojoidu/book/springwithaws/SpringWithAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoidu.book.springwithaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
